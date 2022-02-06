bf10476ff94e4c94896441f6841d376fdf90054f
grandpascripts <98452518+grandpascripts@users.noreply.github.com>
2022-02-05 15:31:25 -1000

    Update whitelist.lua

--- Roblox/DaHood/AltCntrl/whitelist.lua
+++ Roblox/DaHood/AltCntrl/whitelist.lua
@@ -1,5 +1,11 @@
 local hash = {
     "AMyUjMxQTOwgjM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;2712",
     "AO2ATM1IDM4EzM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;143",
+    "wkzNwAzN0QjM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;2929",
+    "QO5QDO1gzN0cjM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;5662",
+    "QOzYTMzEzMzYjM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;3037",
+    "5czNyMDM2kTM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;9688",
+    "zQjN4ADN1YjN? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;9494",
+    "QTM0UzM5IDN? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;7670",
 }
 return hash
