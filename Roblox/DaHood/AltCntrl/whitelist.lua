14bd9a1f3e9595eb24343568ebbdc0ac4df51d93
grandpascripts <98452518+grandpascripts@users.noreply.github.com>
2022-02-22 17:00:11 -1000

    Update whitelist.lua

--- Roblox/DaHood/AltCntrl/whitelist.lua
+++ Roblox/DaHood/AltCntrl/whitelist.lua
@@ -72,5 +72,6 @@ local hash = {
     "AOxkDMwMzN5UTM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;4827",
     "QO1cDN3IjMyAzM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;6526",
     "wM0IzM2MDNzcjM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;8486",
+    "QN4kDMyMDM3IzM? -> String Encrypted using hasher 3.2.A <- ?Compiler: 8_CompiledHore{{--++__==}};;2636",
 }
 return hash
